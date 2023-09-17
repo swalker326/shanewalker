6fff2e98476d5f4d9fd0323cbe29faf6abffa8e4
Shane Walker <swalker@lemongrasscloud.com>
2023-09-17 08:51:59 -0500

    added git ignore and some colors

--- bin.js
+++ bin.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
-console.log("👋 I'm Shane Walker, a web engineer");
-console.log("🌎 I live in the United States 🇺🇸");
-console.log("👨‍👩‍👧‍👧 I'm married and have two daughters #girldad");
-console.log("👨‍💻 I work @Lemongrass building SAP on the cloud");
-console.log("🌱 I'm currently learning about Go");
-console.log("📫 How to reach me: https://twitter.com/swalker326");
+const { default: chalk } = require("chalk");
+
+console.log(
+  `👋 I'm ${chalk.bgBlue("Shane Walker")}, a ${chalk.bgYellow("web engineer")}`
+);
+console.log("🌎 I live in the United States");
+console.log(
+  `I'm married and have ${chalk.magenta("two daughters #girldad")} 👨‍👩‍👧‍👧 `
+);
+console.log(
+  `💻 I work ${chalk.greenBright("@Lemongrass")} building SAP on the cloud`
+);
+console.log(`I'm currently ${chalk.bgCyan("learning about Go")}`);
+console.log("How to reach me: https://x.com/swalker326");
