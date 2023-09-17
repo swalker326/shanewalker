e9e20ee0d7a50e89eb3b2299aeb115525a1ebee9
Shane Walker <swalker@lemongrasscloud.com>
2023-09-17 08:34:43 -0500

    first commit

---
+++ bin.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+console.log("👋 I'm Shane Walker, a web engineer");
+console.log("🌎 I live in the United States 🇺🇸");
+console.log("👨‍👩‍👧‍👧 I'm married and have two daughters #girldad");
+console.log("👨‍💻 I work @Lemongrass building SAP on the cloud");
+console.log("🌱 I'm currently learning about Go");
+console.log("📫 How to reach me: https://twitter.com/swalker326");
